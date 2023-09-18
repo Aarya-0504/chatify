ccc81a780cdcc43c1aa52b4bac7d53d6a0b1b057
Aarya_P <97930406+Aarya-0504@users.noreply.github.com>
2023-09-18 11:49:47 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,14 +8,7 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyA0IEIowjpLvAv5pM0ZPatUfMBas604HeU",
-    authDomain: "chatify-4670f.firebaseapp.com",
-    projectId: "chatify-4670f",
-    storageBucket: "chatify-4670f.appspot.com",
-    messagingSenderId: "609898730457",
-    appId: "1:609898730457:web:7ca48e52514b4e6f97ffc8"
-  };
+
   
   // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
